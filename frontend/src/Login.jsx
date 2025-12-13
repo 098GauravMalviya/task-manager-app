c7bdcc6579074ec6aa486ccec153d03963cacf54
098GauravMalviya <vishwakarmag018@gmail.com>
2025-12-13 19:19:38 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:3000/login", {
+      const res = await axios.post("https://task-manager-backend-uuwk.onrender.com/login", {
         username,
         password,
       });
